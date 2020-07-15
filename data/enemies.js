1be369365da2ed6315b96fcf21396082b25637b2
Sean McPherson <git@seanmcp.com>
2020-07-15 12:38:49 -0400

    feat: add enemies [WIP]

---
+++ data/enemies.js
@@ -0,0 +1,68 @@
+module.export = {
+    /**
+     * AC <= 12
+     * HP <= 15
+     */
+    easy: [
+        [
+            // 1 - 5
+            "rat", // 10/1
+            "spider", // 12/1
+            "lizard", // 10/2
+        ],
+        [
+            // 6 - 10
+            "giant rat", // 12/7
+            "awakened shrub", // 9/10
+        ],
+        [
+            // 11 - 15
+            "boar", // 11/11
+            "giant wasp", // 12/13
+            "panther", // 12/13
+        ]
+    ],
+    /**
+     * AC <= 16
+     * HP <= 20
+     */
+    medium: [
+        [
+            "giant lizard", // 12/19
+            "lion", // 12/26
+            "tiger", // 12/37
+            "giant boar", // 12/42
+            "giant hyena", // 12/45
+            "black bear", // 11/19
+            "giant frog", // 11/18
+            "giant wolf spider", // 13/11
+            "swarm of poisonous snakes", // 14/36
+            "wolf", // 13/11
+            "warg", // 13/26
+            "dire wolf", // 14/37
+            "giant bat", // 13/22
+            "giant spider", // 14/26
+            "giant scorpion", // 15/52
+            "giant fire beetle", // 13/4
+            "giant centipede", // 13/4
+            "poisonous snake", // 13/2
+            "flying snake", // 14/5
+            "swarm of insects", // 12/22
+            "swarm of bats", // 12/22
+            "swarm of rats", // 10/24
+
+        ],
+        [
+
+        ],
+        [
+
+        ]
+    ],
+    hard: [
+        [
+            "giant constrictor snake", // 12/60
+            "giant constrictor crocodile", // 14/85
+        ],
+    ]
+}
